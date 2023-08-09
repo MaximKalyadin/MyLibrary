3b906cb5aa9bf541d1a7e039c10bad0959d4a29e
Maxim_Kalyadin <kalyadin.maxim@gmail.com>
2023-08-09 19:30:11 +0400

    Init

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule, isDevMode } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+
+@NgModule({
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ServiceWorkerModule.register('ngsw-worker.js', {
+            enabled: !isDevMode(),
+        }),
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+})
+export class AppModule {}
